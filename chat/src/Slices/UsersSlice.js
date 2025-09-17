b31c98339c658dce43f8f63c46b3c47d6e7b617e
Ankit Mahajan <ankitmahajan2257@gmail.com>
2025-09-17 18:27:13 +0530

    addusre

--- chat/src/Slices/UsersSlice.js
+++ chat/src/Slices/UsersSlice.js
@@ -1,33 +1,55 @@
-import { createSlice,createAsyncThunk } from "@reduxjs/toolkit"
-import { collection,getDocs } from "firebase/firestore" 
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import { collection, getDocs, addDoc } from "firebase/firestore"
 import { store } from "../firebase/firebase"
 
-const initialState={
-    usres:[],
-    isLoading:true,
-    error:null,
+const initialState = {
+    usres: [],
+    isLoading: true,
+    error: null,
 }
 
 //fetch user
-    const fetchUser=createAsyncThunk("Usre-fetch",async()=>{
-        const querSnapshot= await getDocs(collection(store,"usres"));
-        const users=querSnapshot.map((doc)=>doc.data)
+const fetchUser = createAsyncThunk("Usre-fetch", async () => {
+    const querSnapshot = await getDocs(collection(store, "users"));
+    const users = querSnapshot.map((doc) => doc.data)
 
-        return users
+    return users
+})
+
+// add user
+const addUser = createAsyncThunk("User-add", async (email, password, name) => {
+    const docRef = await addDoc(collection(store, "users"), {
+        email: email,
+        password: password,
+        name: name,
     })
+    return docRef
+})
 
-const userSlice=createSlice({
-    name:"usres",
+const userSlice = createSlice({
+    name: "users",
     initialState,
-    reducers:{},
-    extraReducers:(buildr)=>{
-        buildr.addCase(fetchUser.pending,(state)=>{
+    reducers: {},
+    extraReducers: (buildr) => {
+        buildr.addCase(fetchUser.pending, (state) => {
+            state.isLoading = true
+        }).addCase(fetchUser.fulfilled, (state, action) => {
+            state.usres = action.payload,
+                state.isLoading = false
+        }).addCase(fetchUser.rejected, (state) => {
+            state.error = "Cant fetch users !!";
+            state.isLoading=false;
+        }).addCase(addUser.pending,(state)=>{
             state.isLoading=true
-        }).addCase(fetchUser.fulfilled,(state,action)=>{
-            state.usres=action.payload,
+        }).addCase(addUser.fulfilled,(state,action)=>{
+            const id=action.payload.id
+            const user=action.payload.data();
+
+            state.usres.push({"user-id":id,...user})
             state.isLoading=false
-        }).addCase(fetchUser.rejected,(state,)=>{
-            state.error="Cant fetch users !!";
+        }).addCase(addUser.rejected,(state,action)=>{
+            state.error="geting error while add user !"
+            state.isLoading=false
         })
     }
 })
