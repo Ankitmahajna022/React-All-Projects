f0cf02ebf6fdfcca3217838a198ba514efd53c78
Ankit Mahajan <ankitmahajan2257@gmail.com>
2025-09-24 12:06:17 +0530

    updata

--- whatApp-Chat/src/page/auth/signUp/SignUp.jsx
+++ whatApp-Chat/src/page/auth/signUp/SignUp.jsx
@@ -1,35 +1,35 @@
 import React, { useRef } from 'react'
-import {useDispatch} from "react-redux"
-import {addUser} from "../../../Slices/userSlice"
-import {createChat} from "../../../Slices/chatSlice"
+import { useDispatch } from "react-redux"
+import { addUser } from "../../../Slices/userSlice"
+import { createChat } from "../../../Slices/chatSlice"
 
 function SignUp() {
- const nameRef=useRef()
- const emailRef=useRef()
- const passwordRef=useRef()
+  const nameRef = useRef()
+  const emailRef = useRef()
+  const passwordRef = useRef()
 
- const dispatch=useDispatch()
+  const dispatch = useDispatch()
 
- const handlaSignUp=()=>{
+  const handlaSignUp = () => {
 
-    const name=nameRef.current.value
-    const email=emailRef.current.value
-    const password=passwordRef.current.value
+    const name = nameRef.current.value
+    const email = emailRef.current.value
+    const password = passwordRef.current.value
 
-    dispatch(addUser({name,email,password}))
+    dispatch(addUser({ name, email, password }))
 
- }
+  }
 
   return (
     <div>
-        <h1>Sign Up</h1>
-        <button onClick={()=>{
-          dispatch(createChat())
-        }}></button>
-        <input type="name" placeholder='name' ref={nameRef}/>
-        <input type="email" placeholder='email'ref={emailRef}/>
-        <input type="password" placeholder='password'ref={passwordRef}/>
-        <button onClick={handlaSignUp}>Sign Up</button>
+      <h1>Sign Up</h1>
+      <button onClick={() => {
+        dispatch(createChat())
+      }}></button>
+      <input type="name" placeholder='name' ref={nameRef} />
+      <input type="email" placeholder='email' ref={emailRef} />
+      <input type="password" placeholder='password' ref={passwordRef} />
+      <button onClick={handlaSignUp}>Sign Up</button>
     </div>
   )
 }
